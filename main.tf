e61857b87979bfc5475dd9a34b5623e223596fe4
sua kim <tndk1966@naver.com>
2024-03-13 18:52:52 +0900

    main.tf

--- main.tf
+++ main.tf
@@ -16,3 +16,13 @@ provider "aws" {
 resource "aws_vpc" "web-vpc" {
   cidr_block = "10.0.0.0/16"
 }
+
+# Create instance
+resource "aws_instance" "web-ec2" {
+  ami = "ami-0382ac14e5f06eb95"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "web-ec2"
+  }
+}
