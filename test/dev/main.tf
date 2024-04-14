d228de4615eda1b5bcaa2f4828d75fe369f44b21
sua kim <tndk1966@naver.com>
2024-04-14 23:37:40 +0900

    tfc 설정

--- test/dev/main.tf
+++ test/dev/main.tf
@@ -1,8 +1,15 @@
 terraform {
-  required_providers {
+  required_providers {    # 필요한 제공자(provider) 지정
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
+    }
+  }
+
+  backend "remote" {
+    organization = "project_suah"  # 테라폼 클라우드 조직명
+    workspaces {
+      name = "terraform-dev"  # 사용할 워크스페이스명
     }
   }
 }
@@ -12,4 +19,5 @@ provider "aws" {
   region = var.region
   profile = "admin"
 }
+
 
